6bb5962eb5316c28e0c5644fafc974f8e3896c1f
kase-haruto <0te06559726481t@au.com>
2025-07-21 13:55:49 +0900

    ブレンドモード修正

--- project/Engine/Graphics/Pipeline/BlendMode/BlendMode.h
+++ project/Engine/Graphics/Pipeline/BlendMode/BlendMode.h
@@ -29,19 +29,14 @@ inline D3D12_BLEND_DESC CreateBlendDesc(BlendMode mode){
 	switch (mode){
 		//-------------------------------------------------------------------------
 		case BlendMode::NONE:
-			// 不透明(ブレンド無効)
-			// BlendEnable = FALSE のまま
 			break;
 
 			//-------------------------------------------------------------------------
 		case BlendMode::ALPHA:
-			// アルファブレンド
-			// (SrcAlpha * Src) + (1 - SrcAlpha) * Dst
 			rt0.BlendEnable = true;
 			rt0.SrcBlend = D3D12_BLEND_SRC_ALPHA;
 			rt0.DestBlend = D3D12_BLEND_INV_SRC_ALPHA;
 			rt0.BlendOp = D3D12_BLEND_OP_ADD;
-			// アルファの合成は上書き
 			rt0.SrcBlendAlpha = D3D12_BLEND_ZERO;
 			rt0.DestBlendAlpha = D3D12_BLEND_ONE;
 			rt0.BlendOpAlpha = D3D12_BLEND_OP_ADD;
@@ -49,11 +44,8 @@ inline D3D12_BLEND_DESC CreateBlendDesc(BlendMode mode){
 
 			//-------------------------------------------------------------------------
 		case BlendMode::ADD:
-			// 加算ブレンド
-			// (SrcAlpha * Src) + Dst
-			// もしくは (Src * ONE) + Dst など要件に応じて
 			rt0.BlendEnable = true;
-			rt0.SrcBlend = D3D12_BLEND_SRC_ALPHA;  // または D3D12_BLEND_ONE
+			rt0.SrcBlend = D3D12_BLEND_SRC_ALPHA;
 			rt0.DestBlend = D3D12_BLEND_ONE;
 			rt0.BlendOp = D3D12_BLEND_OP_ADD;
 			rt0.SrcBlendAlpha = D3D12_BLEND_ZERO;
@@ -63,15 +55,10 @@ inline D3D12_BLEND_DESC CreateBlendDesc(BlendMode mode){
 
 			//-------------------------------------------------------------------------
 		case BlendMode::SUB:
-			// 減算ブレンド (Dst - Src)
-			// ここでは (1 * Dst) - (1 * Src) として実装
 			rt0.BlendEnable = true;
 			rt0.SrcBlend = D3D12_BLEND_ONE;
 			rt0.DestBlend = D3D12_BLEND_ONE;
-			// 減算: BlendOp = SUBTRACT の場合は (Src - Dst)
-			//        REV_SUBTRACT で (Dst - Src)
 			rt0.BlendOp = D3D12_BLEND_OP_REV_SUBTRACT;
-			// アルファは上書き
 			rt0.SrcBlendAlpha = D3D12_BLEND_ZERO;
 			rt0.DestBlendAlpha = D3D12_BLEND_ONE;
 			rt0.BlendOpAlpha = D3D12_BLEND_OP_ADD;
@@ -79,13 +66,10 @@ inline D3D12_BLEND_DESC CreateBlendDesc(BlendMode mode){
 
 			//-------------------------------------------------------------------------
 		case BlendMode::MUL:
-			// 乗算ブレンド (Dst * Src)
-			// (SrcBlend=DEST_COLOR, DestBlend=ZERO) → Result = Src * Dst
 			rt0.BlendEnable = true;
 			rt0.SrcBlend = D3D12_BLEND_DEST_COLOR;
 			rt0.DestBlend = D3D12_BLEND_ZERO;
 			rt0.BlendOp = D3D12_BLEND_OP_ADD;
-			// アルファは上書き
 			rt0.SrcBlendAlpha = D3D12_BLEND_ZERO;
 			rt0.DestBlendAlpha = D3D12_BLEND_ONE;
 			rt0.BlendOpAlpha = D3D12_BLEND_OP_ADD;
@@ -93,8 +77,6 @@ inline D3D12_BLEND_DESC CreateBlendDesc(BlendMode mode){
 
 			//-------------------------------------------------------------------------
 		case BlendMode::NORMAL:
-			// 通常ブレンド(ここでは ALPHA と同じ例)
-			// あるいは「100%ソースを描く(不透明)」にしたいなら NONEと同様にするなど
 			rt0.BlendEnable = true;
 			rt0.SrcBlend = D3D12_BLEND_SRC_ALPHA;
 			rt0.DestBlend = D3D12_BLEND_INV_SRC_ALPHA;
@@ -106,14 +88,10 @@ inline D3D12_BLEND_DESC CreateBlendDesc(BlendMode mode){
 
 			//-------------------------------------------------------------------------
 		case BlendMode::SCREEN:
-			// スクリーン近似
-			// 公式には (1 - (1 - Src) * (1 - Dst)) だが固定機能だけでは完全再現が難しい
-			// ここでは「SrcBlend=INV_DEST_COLOR, DestBlend=ONE」で近似
 			rt0.BlendEnable = true;
-			rt0.SrcBlend = D3D12_BLEND_INV_DEST_COLOR; // (1 - DstColor) * Src
-			rt0.DestBlend = D3D12_BLEND_ONE;            // + Dst
+			rt0.SrcBlend = D3D12_BLEND_INV_DEST_COLOR;
+			rt0.DestBlend = D3D12_BLEND_ONE;
 			rt0.BlendOp = D3D12_BLEND_OP_ADD;
-			// アルファブレンドは ALPHA と同じ
 			rt0.SrcBlendAlpha = D3D12_BLEND_SRC_ALPHA;
 			rt0.DestBlendAlpha = D3D12_BLEND_INV_SRC_ALPHA;
 			rt0.BlendOpAlpha = D3D12_BLEND_OP_ADD;
