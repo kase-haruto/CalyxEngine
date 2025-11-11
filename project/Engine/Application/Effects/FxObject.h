3ab46391636d3c8a55a13be2adc5d52fa00a2fe7
kase-haruto <0te06559726481t@au.com>
2025-11-11 10:22:00 +0900

    処理コメントを追加しました。

--- project/Engine/Application/Effects/FxObject.h
+++ project/Engine/Application/Effects/FxObject.h
@@ -10,6 +10,9 @@
 // config
 #include <Data/Engine/Configs/Scene/Objects/Particle/EffectConfig.h>
 
+/* ========================================================================
+/*		エフェクトをシーン上にオブジェクトとして配置するクラス
+/* ===================================================================== */
 class FxObject final
 	: public SceneObject,
 	  public IConfigurable {
@@ -26,17 +29,34 @@ public:
 	void AlwaysUpdate(float dt) override;
 
 	//--------- Player ----------------------------------------------------
+	///<summary>すべてのエミッターを再生</summary>
 	void PlayAll() const;
+	///<summary>すべてのエミッターを停止</summary>
 	void StopAll() const;
+	///<summary>すべてのエミッターをリスタート</summary>
 	void RestartAll() const;
 
 	//--------- debugUi ---------------------------------------------------
 	void ShowGui() override;
 
 	//--------- json ------------------------------------------------------
+	/// <summary>
+	/// コンフィグ適用
+	/// </summary>
 	void ApplyConfig();
+	/// <summary>
+	///	コンフィグ掃き出し
+	/// </summary>
 	void ExtractConfig();
+	/// <summary>
+	///	json空コンフィグの適用
+	/// </summary>
+	/// ///<param name="j"></param>
 	void ApplyConfigFromJson(const nlohmann::json& j) override;
+	///<summary>
+	///jsonに掃き出し
+	///</summary>
+	///<param name="j"></param>
 	void ExtractConfigToJson(nlohmann::json& j) const override;
 
 	//--------- accessor --------------------------------------------------
