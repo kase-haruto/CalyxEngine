d52ea03746801c8b88912cdbc6b8d367e2a3943f
kase-haruto <0te06559726481t@au.com>
2025-11-20 11:29:17 +0900

    SceneManagerにあった使用していない不要な関数を削除しました

--- project/Engine/Objects/3D/Actor/SceneObjectManager.h
+++ project/Engine/Objects/3D/Actor/SceneObjectManager.h
@@ -8,36 +8,29 @@
  */
 class SceneObjectManager {
 public:
-	//==================================================================*//
-	//			singleton
-	//==================================================================*//
-	SceneObjectManager(const SceneObjectManager&)			   = delete;
-	SceneObjectManager& operator=(const SceneObjectManager&)   = delete;
-	SceneObjectManager(SceneObjectManager&&)				   = delete;
-	SceneObjectManager&		   operator=(SceneObjectManager&&) = delete;
-	static SceneObjectManager* GetInstance();
+	SceneObjectManager(); // コンストラクタ
+	~SceneObjectManager()                                    = default;
+	SceneObjectManager(const SceneObjectManager&)            = delete;
+	SceneObjectManager& operator=(const SceneObjectManager&) = delete;
+	SceneObjectManager(SceneObjectManager&&)                 = delete;
+	SceneObjectManager& operator=(SceneObjectManager&&)      = delete;
 
 public:
 	//==================================================================*//
 	//			public functions
 	//==================================================================*//
-	void ClearAllObject();	 // 全てのオブジェクトを削除
-	void ClearGameObjects(); // GameObjectのみを削除
-	void Finalize();		 // 終了処理
-
-	// objectの取得追加
-	void							 AddObject(SceneObject* object);
-	const std::vector<SceneObject*>& GetAllObjects() const { return allSceneObjects_; }
-
-	// objectの解放処理
-	void RemoveObject(SceneObject* object);
-
-private:
-	//==================================================================*//
-	//			private functions
-	//==================================================================*//
-	SceneObjectManager();			 // コンストラクタ
-	~SceneObjectManager() = default; // デストラクタ
+	/**
+	 * \brief すべてのオブジェクトを削除
+	 */
+	void ClearAllObject();
+	/**
+	 * \brief gameObjectのみを削除
+	 */
+	void ClearGameObjects();
+	/**
+	 * \brief 終了処理
+	 */
+	void Finalize();
 
 private:
 	//==================================================================*//
