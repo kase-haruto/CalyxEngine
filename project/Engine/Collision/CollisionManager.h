fa54fe8b5e616477afe3c254f79fc5ded4c12510
kase haruto <k023g0052@g.neec.ac.jp>
2026-03-09 04:46:41 +0900

    同じ名前のアクタがあっても衝突を正しく取れるように修正しました。

--- project/Engine/Collision/CollisionManager.h
+++ project/Engine/Collision/CollisionManager.h
@@ -4,22 +4,22 @@
 
 // c++
 #include <list>
-#include <vector>
 #include <unordered_set>
+#include <vector>
 
 /*-----------------------------------------------------------------------------------------
  * CollisionManager
  * - コリジョンマネージャークラス
  * - シーン内の全コライダーの登録・総当たり判定・衝突イベント発行を担当
  *---------------------------------------------------------------------------------------*/
-class CollisionManager{
+class CollisionManager {
 public:
 	using CollisionShape = std::variant<Sphere, OBB>;
 	//===================================================================*/
 	//                   singleton
 	//===================================================================*/
 	static CollisionManager* GetInstance();
-	CollisionManager(const CollisionManager&) = delete;
+	CollisionManager(const CollisionManager&)			 = delete;
 	CollisionManager& operator=(const CollisionManager&) = delete;
 
 public:
@@ -27,14 +27,29 @@ public:
 	//===================================================================*/
 	//                   public functions
 	//===================================================================*/
-	void UpdateCollisionAllCollider();			// すべてのコライダーを総当たりで判定
+	void UpdateCollisionAllCollider(); // すべてのコライダーを総当たりで判定
 
-	Collider* FindColliderByName(const std::string& name);
-
-	void Register(Collider* collider);		// コライダーリストに追加
-	void Unregister(Collider* collider);	// コライダーリストから削除
+	void Register(Collider* collider);	 // コライダーリストに追加
+	void Unregister(Collider* collider); // コライダーリストから削除
 	void DebugLog();
 	void ClearColliders();
+
+	struct CollisionPair {
+		Collider* a;
+		Collider* b;
+
+		bool operator==(const CollisionPair& other) const {
+			return (a == other.a && b == other.b) || (a == other.b && b == other.a);
+		}
+	};
+
+	struct CollisionPairHash {
+		size_t operator()(const CollisionPair& pair) const {
+			auto h1 = std::hash<const Collider*>{}(pair.a);
+			auto h2 = std::hash<const Collider*>{}(pair.b);
+			return h1 ^ h2;
+		}
+	};
 
 private:
 	//===================================================================*/
@@ -44,14 +59,13 @@ private:
 	~CollisionManager() = default;
 
 	bool CheckCollisionPair(Collider* colliderA, Collider* colliderB);
-	std::string MakeCollisionKey(Collider* colliderA, Collider* colliderB);
 
-	void ComputeOBBAxes(const OBB& obb, CalyxMath::Vector3 outAxis[3]);
+	void  ComputeOBBAxes(const OBB& obb, CalyxMath::Vector3 outAxis[3]);
 	float ProjectOBB(const OBB& obb, const CalyxMath::Vector3 obbAxes[3], const CalyxMath::Vector3& axisCandidate);
-	bool OverlapOnAxis(
-		const OBB& obbA, const CalyxMath::Vector3 aAxes[3],
-		const OBB& obbB, const CalyxMath::Vector3 bAxes[3],
-		const CalyxMath::Vector3& axisCandidate);
+	bool  OverlapOnAxis(
+		 const OBB& obbA, const CalyxMath::Vector3 aAxes[3],
+		 const OBB& obbB, const CalyxMath::Vector3 bAxes[3],
+		 const CalyxMath::Vector3& axisCandidate);
 
 	/*----------------
 	 各形状ごとの衝突
@@ -60,15 +74,12 @@ private:
 	bool SphereToOBB(const Sphere& sphere, const OBB obb);
 	bool OBBToOBB(const OBB& obbA, const OBB& obbB);
 
-
 private:
 	//===================================================================*/
 	//                   private variable
 	//===================================================================*/
-	std::list<Collider*>colliders_;
-	std::vector<std::string> collisionLogs_; // 衝突ログ
-
-	std::unordered_set<std::string> currentCollisions_;  // 現在のフレームの衝突ペア
-	std::unordered_set<std::string> previousCollisions_; // 前のフレームの衝突ペア
+	std::list<Collider*>								 colliders_;
+	std::vector<std::string>							 collisionLogs_;	  // 衝突ログ
+	std::unordered_set<CollisionPair, CollisionPairHash> currentCollisions_;  // 現在のフレームの衝突ペア
+	std::unordered_set<CollisionPair, CollisionPairHash> previousCollisions_; // 前のフレームの衝突ペア
 };
-
