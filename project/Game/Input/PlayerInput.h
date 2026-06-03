795b1d2c0f23bc4adcf6143b2d74334892ad0ab1
kase haruto <k023g0052@g.neec.ac.jp>
2026-06-04 00:52:01 +0900

    playerInputを追加しました

---
+++ project/Game/Input/PlayerInput.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+
+#include "IInputAction.h"
+#include <Game/3D/Actor/DemoPlayer/Details/DemoPlayerActionConfig.h>
+
+/**
+ * \brief プレイヤー入力管理クラス
+ * - キーボード、ゲームパッドの入力をInputActionに統合
+ * - キーバインディングは動的に変更可能
+ */
+class PlayerInput :
+public BaseInputAction<InputAction> {
+public:
+	PlayerInput();
+	~PlayerInput() override = default;
+
+	/**
+	 * \brief ImGuiでキーバインディングを表示・編集
+	 */
+	void ShowGui();
+
+	/**
+	 * \brief キーバインディングを初期化
+	 */
+	void ResetBindings() override;
+};
+
+
