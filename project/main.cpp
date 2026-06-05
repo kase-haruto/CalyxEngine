8e4631b2d93a703e0cff899ba412ef634e565186
kase haruto <k023g0052@g.neec.ac.jp>
2026-06-05 21:24:43 +0900

    gameappを別ファイルに移動させました。

--- project/main.cpp
+++ project/main.cpp
@@ -1,20 +1,6 @@
 #include <CalyxEngine/CalyxEngine.h>
 
-#include <Demo/Scene/Utility/SceneTypeUtil.h>
-#include <Demo/Scene/DemoScene/DemoScene.h>
-
-class GameApplication : public Calyx::Application {
-public:
-	void RegisterScenes(Calyx::SceneRegistry& registry) override {
-		registry.AddScene<DemoScene>(GameSceneUtil::ToSceneId(SceneType::DEMO));
-		registry.SetStartupScene(GameSceneUtil::ToSceneId(SceneType::DEMO));
-	}
-
-	void OnInitialize() override {}
-	void OnUpdate() override {}
-	void OnRender() override {}
-	void OnFinalize() override {}
-};
+#include "GameApplication.h"
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR commandLine, int){
 	GameApplication application;
